65bf51e897fc8b55273256d2c224a952005529fd
Leonchik1945 <leonid.laskavyy@icloud.com>
2021-11-07 11:49:14 +0300

    HW 5 upload

--- 2021.11.01_homework/Project3/Source.cpp
+++ 2021.11.01_homework/Project3/Source.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	double d = 0;
-	cin >> d;
+	double r = 0;
+	cin >> r;
 	long long int value = 0;
-	value = *((long long int*) & d);
+	value = *((long long int*) & r);
 	long long int BitMask = 1;
 	BitMask = BitMask << (sizeof(long long int) * 8 - 1);
 	BitMask = ~BitMask;
