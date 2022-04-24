62dd8efe117064f1dccc4139fbedb297b058e8c9
Leonchik1945 <leonid.laskavyy@icloud.com>
2022-04-24 21:04:05 +0300

    HW 2 upload

---
+++ 2022.04.25_homework/Project1/Source.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include "ArrayList.h"
+
+using namespace std;
+
+int main()
+{
+	ArrayList list(5);
+	cout << list << endl;
+	list.pushend(5);
+	cout << list << endl;
+	list.pushend(4);
+	list.pushend(3);
+	list.pushend(2);
+	list.pushend(1);
+	cout << list << endl;
+	list.pushend(0);
+	cout << list << endl;
+	cout << list.extractend();
+	cout << list << endl;
+
+	list[1] = 1;
+	cout << list << endl;
+	list[5] = 10;
+	cout << list << endl;
+	list[16] = 20;
+	cout << list << endl;
+	list[-2] = 20;
+	cout << list << endl;
+
+	list.pushend(10);
+	list.insert(20, 2);
+	list.sort();
+	cout << list << endl;
+
+	return EXIT_SUCCESS;
+}
